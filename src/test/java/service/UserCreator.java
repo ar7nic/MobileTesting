c211de77a4e03c8d28b80da7897356436c0d8d68
Ar7nic <ar7nic@gmail.com>
2022-09-20 18:23:17 +0300

    ready for jenkins

--- src/test/java/service/UserCreator.java
+++ src/test/java/service/UserCreator.java
@@ -9,18 +9,18 @@ public class UserCreator {
 
     public static User withCredentialsFromProperty(){
 
-        return new User("my.testing.aqa@gmail.com", "Testinggmail");
-//        return new User(TestDataReader.getTestData(TESTDATA_USER_LOGIN),
-//                TestDataReader.getTestData(TESTDATA_USER_PASSWORD));
+//        return new User("my.testing.aqa@gmail.com", "Testinggmail");
+        return new User(TestDataReader.getTestData(TESTDATA_USER_LOGIN),
+                TestDataReader.getTestData(TESTDATA_USER_PASSWORD));
     }
 
     public static User withWrongUsername(){
-        return new User("1jf7hdg64@gmail.com", "Testinggmail");
-//        return new User("1jf7hdg64@gmail.com", TestDataReader.getTestData(TESTDATA_USER_PASSWORD));
+//        return new User("1jf7hdg64@gmail.com", "Testinggmail");
+        return new User("1jf7hdg64@gmail.com", TestDataReader.getTestData(TESTDATA_USER_PASSWORD));
     }
 
     public static User withWrongPassword(){
-        return new User("my.testing.aqa@gmail.com", "ssdfgh");
-//        return new User(TestDataReader.getTestData(TESTDATA_USER_LOGIN), "ssdfgh");
+//        return new User("my.testing.aqa@gmail.com", "ssdfgh");
+        return new User(TestDataReader.getTestData(TESTDATA_USER_LOGIN), "ssdfgh");
     }
 }
